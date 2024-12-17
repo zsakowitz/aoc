f7daacc3ea7d19dfe329f4319bfe4d96d75fff51
sakawi <65452710+zsakowitz@users.noreply.github.com>
2024-12-16 22:53:48 -0800

    cries

--- 2024/17.js
+++ 2024/17.js
@@ -1,36 +1,19 @@
 import "../util.js"
 
 const s = "2,4,1,5,7,5,4,5,0,3,1,6,5,5,3,0"
-const d = s.split(",").int()
 
 function go(a) {
   return go() == 2 && a != 0
 
-  //   for (const v of d.slice(0, -1)) {
-  //     const r = go()
-  //     if (v != r) return false
-  //     if (a == 0) return false
-  //   }
-  //
-  //   const r = go()
-  //   if (d.last != r) return false
-  //   return a == 0
-
   function go() {
-    // const ret = (a % 8 ^ 0b11 ^ ((a >> 3) >> (a % 8 ^ 0b101))) % 8
-    // a >>= 3
-    // return ret
-
-    let b = a % 8
-    const v = (b ^ 0b101) % 8 ^ (a >> (b ^ 0b101)) % 8 ^ 0b110
+    const v = a % 8 ^ 0b011 ^ (a >> (a % 8 ^ 0b101)) % 8
     a = a >> 3
     return v
   }
 }
 
 const q = [
-  1840894, 1840897, 1840905, 1840928, 1840936, 1840944, 1840952, 1840958,
-  1841022, 1841025, 1841033,
+  806456, 806462, 806526, 806529, 806531, 806537, 806539, 806547, 806555,
 ]
 const last = q.last
 
